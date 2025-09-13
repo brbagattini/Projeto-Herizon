1324438401db860207e204562d1c86f7f3e92c39
Bruno Bagattini Fernandes <brbagattinif@gmail.com>
2025-09-13 19:11:33 -0300

    Feat: Sobre nos

--- src/routes/SobreNos.jsx
+++ src/routes/SobreNos.jsx
@@ -10,6 +10,12 @@ function SobreNos() {
       <div className='header'>
         <a href="/clubes"><p>Voltar</p></a>
       </div>
+      <div className='HistoriaSlideshow'>
+        <img src="" alt="" />
+      </div>
+      <div className='TextHistoria'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et perferendis soluta qui aut. Explicabo maxime eum, quam recusandae eaque saepe voluptate neque culpa. Maxime assumenda odio expedita, nemo facere ad.</p>
+      </div>
     </main>
     </>
   )
